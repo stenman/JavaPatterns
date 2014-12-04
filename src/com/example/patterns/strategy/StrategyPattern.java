7daef6dd66cc61e45406a81f5611a4e0c8fe7810
stenman <roger.stenman@gmail.com>
2014-12-04 20:47:03 +0100

    Testcommit

--- src/com/example/patterns/strategy/StrategyPattern.java
+++ src/com/example/patterns/strategy/StrategyPattern.java
@@ -3,5 +3,7 @@ package com.example.patterns.strategy;
 public class StrategyPattern {
 	public static void main(String[] args) {
 		System.out.println("Hello Strategy Pattern!");
+		
+		System.out.println("Another line");
 	}
 }
