cb968d39d707bed0a584aa6a1607a72fbbe198ad
stenman <roger.stenman@gmail.com>
2015-02-01 21:53:08 +0100

    Added Singleton pattern

---
+++ src/com/example/patterns/singleton/GetTheTiles.java
@@ -0,0 +1,18 @@
+package com.example.patterns.singleton;
+
+import java.util.LinkedList;
+
+public class GetTheTiles implements Runnable {
+
+	@Override
+	public void run() {
+		Singleton newInstance = Singleton.getInstance();
+		System.out.println("Instance ID: " + System.identityHashCode(newInstance));
+		System.out.println(newInstance.getLetterList());
+		LinkedList<String> playerTiles = newInstance.getTiles(7);
+		System.out.println("Player: " + playerTiles);
+
+		System.out.println("Got Tiles\n");
+	}
+
+}
---
+++ src/com/example/patterns/singleton/SingletonPatternWithThreads.java
@@ -0,0 +1,12 @@
+package com.example.patterns.singleton;
+
+public class SingletonPatternWithThreads {
+
+	public static void main(String[] args) {
+		Runnable getTiles = new GetTheTiles();
+		Runnable getTilesAgain = new GetTheTiles();
+		new Thread(getTiles).start();
+		new Thread(getTilesAgain).start();
+	}
+
+}
