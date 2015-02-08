13475ae76a20e5a9ac0290d08e7962179fc5efb0
stenman <roger.stenman@gmail.com>
2015-02-08 22:33:47 +0100

    Added werewolf to the Builder pattern

--- src/com/example/patterns/builder/BuilderPattern.java
+++ src/com/example/patterns/builder/BuilderPattern.java
@@ -4,19 +4,31 @@ public class BuilderPattern {
 
 	public static void main(String[] args) {
 		MonsterBuilder frankensteinsMonster = new FrankensteinsMonsterBuilder();
+		MonsterBuilder werewolfMonster = new WerewolfMonsterBuilder();
 
-		MonsterEngineer monsterEngineer = new MonsterEngineer(frankensteinsMonster);
+		MonsterEngineer monsterEngineer;
 
+		monsterEngineer = new MonsterEngineer(frankensteinsMonster);
 		monsterEngineer.makeMonster();
-
 		Monster firstMonster = monsterEngineer.getMonster();
 
-		System.out.println("Monster built!");
+		System.out.println("\nFirst monster built!");
 
 		System.out.println("Monster head type: " + firstMonster.getMonsterHead());
 		System.out.println("Monster torso type: " + firstMonster.getMonsterTorso());
 		System.out.println("Monster arms type: " + firstMonster.getMonsterArms());
 		System.out.println("Monster legs type: " + firstMonster.getMonsterLegs());
+
+		monsterEngineer = new MonsterEngineer(werewolfMonster);
+		monsterEngineer.makeMonster();
+		Monster secondMonster = monsterEngineer.getMonster();
+
+		System.out.println("\nSecond monster built!");
+
+		System.out.println("Monster head type: " + secondMonster.getMonsterHead());
+		System.out.println("Monster torso type: " + secondMonster.getMonsterTorso());
+		System.out.println("Monster arms type: " + secondMonster.getMonsterArms());
+		System.out.println("Monster legs type: " + secondMonster.getMonsterLegs());
 	}
 
 }
---
+++ src/com/example/patterns/builder/WerewolfMonsterBuilder.java
@@ -0,0 +1,38 @@
+package com.example.patterns.builder;
+
+public class WerewolfMonsterBuilder implements MonsterBuilder{
+
+	private Monster monster;
+
+	public WerewolfMonsterBuilder() {
+		this.monster = new Monster();
+	}
+
+	@Override
+	public void buildMonsterHead() {
+		monster.setMonsterHead("Hairy head with pointy teeth");
+
+	}
+
+	@Override
+	public void buildMonsterTorso() {
+		monster.setMonsterTorso("Hairy and muscular");
+	}
+
+	@Override
+	public void buildMonsterArms() {
+		monster.setMonsterArms("Hairy with claws");
+	}
+
+	@Override
+	public void buildMonsterLegs() {
+		monster.setMonsterLegs("Hairy with claws");
+	}
+
+	@Override
+	public Monster getMonster() {
+		return this.monster;
+	}
+
+	
+}
